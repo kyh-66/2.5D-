91ea3106a43336ed98e4dd99d13a577c40139a53
349359 <3493598861@qq.com>
2026-02-13 11:51:15 +0800

    第二次

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private int speed;
+    [SerializeField] private Animator anim;
+    [SerializeField] private SpriteRenderer playerSprite;
+
+
+    private PlayerControls playerControls;
+    private Rigidbody rb;
+    private Vector3 movement;
+
+    private const string IS_WALK_PARAM = "IsWalk";
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x = playerControls.Player.Move.ReadValue<Vector2>().x;
+        float z = playerControls.Player.Move.ReadValue<Vector2>().y;
+
+        movement = new Vector3(x, 0, z).normalized;
+        anim.SetBool(IS_WALK_PARAM, movement != Vector3.zero);
+
+        if(x!=0 && x < 0)
+        {
+            playerSprite.flipX = true;
+        }
+        if(x!= 0 && x > 0)
+        {
+            playerSprite.flipX = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
